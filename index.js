cb8c9adf457bcfc9065ee789d3ee884e410206a1
nick <mwauranicholas10@gmail.com>
2022-12-15 08:25:53 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,7 @@
 function addingEventListener() {
+    const input = document.getElementById('input');
+    function clickAlert() {
+        alert('Hee hee that tickles!');
+    }
+    input.addEventListener('click', clickAlert);
 }
